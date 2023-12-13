96f61539d39bc96940f0794a1a031dd22663db60
PRANJAL KAR <74347116+pranjalkar99@users.noreply.github.com>
2023-12-13 16:21:44 +0000

    Basic

--- Hello.py
+++ Hello.py
@@ -1,51 +1,28 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+def find_largest_number(num1, num2, num3):
+    numbers = [num1, num2, num3]
+    return max(numbers)
 
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
+def main():
+    st.title("Largest Number Finder")
+    
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+        Welcome to the Largest Number Finder app for Week 8 TDS Assignment! 
+        This simple app takes three numbers as input and finds the largest among them. 
+        Give it a try by entering three numbers below and see if it helps with your assignment.
+        """
     )
 
+    num1 = st.number_input("Enter the first number:")
+    num2 = st.number_input("Enter the second number:")
+    num3 = st.number_input("Enter the third number:")
+
+    if st.button("Find Largest"):
+        result = find_largest_number(num1, num2, num3)
+        st.success(f"The largest number is: {result}")
 
 if __name__ == "__main__":
-    run()
+    main()
+
